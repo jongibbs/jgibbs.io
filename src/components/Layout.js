2e5d45da7dd1ec58a1cfac44483377eb710c20d6
Ryan Barnett <rmbrntt@gmail.com>
2018-11-11 14:57:52 -0500

    cleaning up the boilerplate from netlify

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | jgibbs.io" />
     <Navbar />
     <div>{children}</div>
   </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,55 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import instagram from '../img/instagram.svg'
+import linkedin from '../img/linkedin.svg'
+import logo from '../img/logo_transparent.png'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+            jgibbs.io
         </Link>
       </div>
-      <div className="navbar-start">
+      {/* <div className="navbar-start">
         <Link className="navbar-item" to="/about">
           About
         </Link>
         <Link className="navbar-item" to="/products">
           Products
         </Link>
-      </div>
+      </div> */}
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/jongibbs"
           target="_blank"
           rel="noopener noreferrer"
         >
           <span className="icon">
             <img src={github} alt="Github" />
+          </span>
+        </a>
+        <a
+          className="navbar-item"
+          href="https://instagram.com/the_jon_gibbs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={instagram} alt="Instagram" />
+          </span>
+        </a>
+        <a
+          className="navbar-item"
+          href="https://linkedin.com/in/gibbsjp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={linkedin} alt="LinkedIn" />
           </span>
         </a>
       </div>
